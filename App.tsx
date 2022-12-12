b427e9f4e8b8cfc7107e3ed6edc189199c6de4d1
Rafael Dias <rafaelldiias100@gmail.com>
2022-12-11 21:09:06 -0300

    feat: layout inicial do componente history graphic

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StyleSheet, SafeAreaView, StatusBar, Platform } from "react-native";
 import CurrentPrice from "./src/components/CurrentPrice";
+import HistoryGraphic from "./src/components/HistoryGraphic";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor="#f50d41" barStyle="light-content" />
       <CurrentPrice />
+      <HistoryGraphic />
     </SafeAreaView>
   );
 }
