2f221145e1fd7a95513e7403730feee16a1d2af8
Rafael Dias <rafaelldiias100@gmail.com>
2022-12-12 16:49:03 -0300

    feat: criando a páginas com React Navigation

--- App.tsx
+++ App.tsx
@@ -1,26 +1,28 @@
-import { StyleSheet, SafeAreaView, StatusBar, Platform } from "react-native";
-import CurrentPrice from "./src/components/CurrentPrice";
-import HistoryGraphic from "./src/components/HistoryGraphic";
-import QuotationsList from "./src/components/QuotationsList";
-import QuotationsItems from "./src/components/QuotationsList/QuotationsItems";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import CryptoInfo from "./src/screens/CryptoInfo";
+import Home from "./src/screens/Home";
+import { StackParams } from "./src/types/StackParams";
+
+const Stack = createStackNavigator<StackParams>();
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar backgroundColor="#f50d41" barStyle="light-content" />
-      <CurrentPrice />
-      <HistoryGraphic />
-      <QuotationsList />
-      <QuotationsItems />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: { backgroundColor: "orange" },
+          headerTitleStyle: { color: "#fff" },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: "CryptoApp" }}
+        />
+        <Stack.Screen name="CryptoInfo" component={CryptoInfo} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#000",
-    alignItems: "center",
-    paddingTop: Platform.OS === "android" ? 40 : 0,
-  },
-});
