c38c25989c833e39190cccf270c0b414fd0ff672
Rafael Dias <rafaelldiias100@gmail.com>
2022-12-13 13:43:44 -0300

    fix: mudança na formatação do preço para corrigir os centavos

--- src/utils/formatPrice.ts
+++ src/utils/formatPrice.ts
@@ -1,6 +1,6 @@
 export function formatPrice(price: number) {
   if (price.toString().includes(".")) {
-    return price.toString().replace(".", ",");
+    return price.toFixed(2).toString().replace(".", ",");
   } else {
     return price.toFixed(2).replace(".", ",");
   }
